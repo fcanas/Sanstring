deb3d67d049fce2122384ee8aef7469ed1990f08
Fabian Canas <fcanas@gmail.com>
2014-12-24 11:27:11 -0500

    Initial commit

---
+++ Sanstring/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Sanstring
+//
+//  Created by Fabian Canas on 12/24/14.
+//  Copyright (c) 2014 Fabian Canas. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Sanstring/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Sanstring
+//
+//  Created by Fabian Canas on 12/24/14.
+//  Copyright (c) 2014 Fabian Canas. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

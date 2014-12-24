8cd019df17b491e907a39f73110f165255f5b2c1
Fabian Canas <fcanas@gmail.com>
2014-12-24 12:21:31 -0500

    Add find-and-replace regular expressions.

--- Sanstring/ViewController.swift
+++ Sanstring/ViewController.swift
@@ -8,11 +8,11 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
+class ViewController: NSViewController, NSTextViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("textDidChange:"), name: NSControlTextDidChangeNotification, object: nil)
         // Do any additional setup after loading the view.
     }
 
@@ -21,7 +21,17 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
-
-
+    
+    @IBOutlet var inputTextView: NSTextView!
+    @IBOutlet weak var regexTextField: NSTextField!
+    @IBOutlet weak var replaceTextField: NSTextField!
+    @IBOutlet var outputTextView: NSTextView!
+    
+    func textDidChange(notification: NSNotification) {
+        let e = NSErrorPointer()
+        let regex = NSRegularExpression(pattern: regexTextField.stringValue, options: nil, error: e)
+        let inputString = inputTextView.string ?? ""
+        outputTextView.string = regex?.stringByReplacingMatchesInString(inputString, options: nil, range: NSMakeRange(0, countElements(inputString)), withTemplate: replaceTextField.stringValue) ?? ""
+    }
 }
 

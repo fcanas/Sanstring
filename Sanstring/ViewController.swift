9ba4d5251942b3c907db7393678a495bf9d782c8
Fabian Canas <fcanas@gmail.com>
2015-10-06 12:58:56 -0400

    Remove text delegation

--- Sanstring/ViewController.swift
+++ Sanstring/ViewController.swift
@@ -8,7 +8,7 @@
 
 import Cocoa
 
-class ViewController: NSViewController, NSTextViewDelegate {
+class ViewController: NSViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()

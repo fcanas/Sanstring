e8a3be2bf7a164c43f64c89729591e1fe2793786
Fabian Canas <fcanas@gmail.com>
2015-10-08 15:01:57 -0400

    Refactor RegexOptionViewController

--- Sanstring/RegexOptionViewController.swift
+++ Sanstring/RegexOptionViewController.swift
@@ -12,47 +12,42 @@ class RegexOptionViewController: NSViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        caseInsensitiveButton.state = (GlobalRegexOptions.rawValue & NSRegularExpressionOptions.CaseInsensitive.rawValue) > 0 ? 1 : 0
-        whitespaceButton.state = (GlobalRegexOptions.rawValue & NSRegularExpressionOptions.AllowCommentsAndWhitespace.rawValue) > 0 ? 1 : 0
-        ignoreMetaCharactersButton.state = (GlobalRegexOptions.rawValue & NSRegularExpressionOptions.IgnoreMetacharacters.rawValue) > 0 ? 1 : 0
-        dotMatchesLineSeparatorsButton.state = (GlobalRegexOptions.rawValue & NSRegularExpressionOptions.DotMatchesLineSeparators.rawValue) > 0 ? 1 : 0
-        anchorsMatchLinesButton.state = (GlobalRegexOptions.rawValue & NSRegularExpressionOptions.AnchorsMatchLines.rawValue) > 0 ? 1 : 0
-        useUnixLineSeparatorsButton.state = (GlobalRegexOptions.rawValue & NSRegularExpressionOptions.UseUnixLineSeparators.rawValue) > 0 ? 1 : 0
-        useUnicodeWordBoundariesButton.state = (GlobalRegexOptions.rawValue & NSRegularExpressionOptions.UseUnicodeWordBoundaries.rawValue) > 0 ? 1 : 0
+        
+        _ = [caseInsensitiveButton,
+            whitespaceButton,
+            ignoreMetaCharactersButton,
+            dotMatchesLineSeparatorsButton,
+            anchorsMatchLinesButton,
+            useUnixLineSeparatorsButton,
+            useUnicodeWordBoundariesButton].map {
+                $0.state = (GlobalRegexOptions.rawValue & option($0)!.rawValue) > 0 ? 1 : 0
+        }
+    }
+    
+    func option(control: NSButton) -> NSRegularExpressionOptions? {
+        return [caseInsensitiveButton      : NSRegularExpressionOptions.CaseInsensitive,
+            whitespaceButton               : NSRegularExpressionOptions.AllowCommentsAndWhitespace,
+            ignoreMetaCharactersButton     : NSRegularExpressionOptions.IgnoreMetacharacters,
+            dotMatchesLineSeparatorsButton : NSRegularExpressionOptions.DotMatchesLineSeparators,
+            anchorsMatchLinesButton        : NSRegularExpressionOptions.AnchorsMatchLines,
+            useUnixLineSeparatorsButton    : NSRegularExpressionOptions.UseUnixLineSeparators,
+            useUnicodeWordBoundariesButton : NSRegularExpressionOptions.UseUnicodeWordBoundaries][control]
     }
     
     @IBOutlet weak var caseInsensitiveButton: NSButton!
-    @IBAction func toggleCaseSensitivity(sender: NSButton) {
-        GlobalRegexOptions = bitSetter(sender.state)(GlobalRegexOptions, NSRegularExpressionOptions.CaseInsensitive)
+    @IBAction func toggleOption(sender: NSButton) {
+        GlobalRegexOptions = bitSetter(sender.state)(GlobalRegexOptions, option(sender)!)
     }
     
     @IBOutlet weak var whitespaceButton: NSButton!
-    @IBAction func toggleWhitespace(sender: NSButton) {
-        GlobalRegexOptions = bitSetter(sender.state)(GlobalRegexOptions, NSRegularExpressionOptions.AllowCommentsAndWhitespace)
-    }
     
     @IBOutlet weak var ignoreMetaCharactersButton: NSButton!
-    @IBAction func toggleIgnoreMetaCharacters(sender: NSButton) {
-        GlobalRegexOptions = bitSetter(sender.state)(GlobalRegexOptions, NSRegularExpressionOptions.IgnoreMetacharacters)
-    }
     
     @IBOutlet weak var dotMatchesLineSeparatorsButton: NSButton!
-    @IBAction func toggleDotMatchesLineSeparatos(sender: NSButton) {
-        GlobalRegexOptions = bitSetter(sender.state)(GlobalRegexOptions, NSRegularExpressionOptions.DotMatchesLineSeparators)
-    }
     
     @IBOutlet weak var anchorsMatchLinesButton: NSButton!
-    @IBAction func toggleAnchorsMatchLines(sender: NSButton) {
-        GlobalRegexOptions = bitSetter(sender.state)(GlobalRegexOptions, NSRegularExpressionOptions.AnchorsMatchLines)
-    }
     
     @IBOutlet weak var useUnixLineSeparatorsButton: NSButton!
-    @IBAction func toggleUseUnixLineSeparators(sender: NSButton) {
-        GlobalRegexOptions = bitSetter(sender.state)(GlobalRegexOptions, NSRegularExpressionOptions.UseUnixLineSeparators)
-    }
     
     @IBOutlet weak var useUnicodeWordBoundariesButton: NSButton!
-    @IBAction func toggleUseUnicodeWordBoundaries(sender: NSButton) {
-        GlobalRegexOptions = bitSetter(sender.state)(GlobalRegexOptions, NSRegularExpressionOptions.UseUnicodeWordBoundaries)
-    }
 }
